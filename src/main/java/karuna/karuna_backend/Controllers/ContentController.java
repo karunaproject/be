98480b67a4c0c53a1e60c5f1c024a1519868344a
Luzu4 <maciekluz@gmail.com>
2024-04-28 21:12:07 +0200

    ticket-1 | swagger description for GetContentByPage

--- src/main/java/karuna/karuna_backend/Controllers/ContentController.java
+++ src/main/java/karuna/karuna_backend/Controllers/ContentController.java
@@ -1,5 +1,9 @@
 package karuna.karuna_backend.Controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import karuna.karuna_backend.DTO.ContentDTO;
 import karuna.karuna_backend.Services.ContentService;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +21,12 @@ public class ContentController {
 
     private final ContentService contentService;
 
+    @Operation(summary = "Get content by page", description = "Fetches content by page name, even if page doesn't " +
+            "exist it will always return default content")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Content for page",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ContentDTO.class)))
     @GetMapping("/{page}")
     public List<ContentDTO> getContentByPage(@PathVariable String page) {
         return contentService.getContentByPage(page);
