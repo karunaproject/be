e5a6bc69979c1caedb35f8507607f2850ac31098
user <bartosz.switala.01@gmail.com>
2024-05-02 00:58:28 +0200

    Refactoring errorResponses into records

--- src/main/java/karuna/karuna_backend/Errors/DTO/AuthenticationErrorResponse.java
+++ src/main/java/karuna/karuna_backend/Errors/DTO/AuthenticationErrorResponse.java
@@ -1,14 +1,4 @@
 package karuna.karuna_backend.Errors.DTO;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-public class AuthenticationErrorResponse {
-    private String key;
-    private String description;
-}
+public record AuthenticationErrorResponse(String key, String description) { }
--- src/main/java/karuna/karuna_backend/Errors/DTO/CustomErrorResponse.java
+++ src/main/java/karuna/karuna_backend/Errors/DTO/CustomErrorResponse.java
@@ -1,14 +1,4 @@
 package karuna.karuna_backend.Errors.DTO;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-public class CustomErrorResponse {
-    private String key;
-    private String description;
-
-}
+public record CustomErrorResponse(String key, String description) { }
