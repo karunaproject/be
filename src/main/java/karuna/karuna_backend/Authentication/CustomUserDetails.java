103282648b28cb9aa6be9a5f71a1cf15bd786cc2
user <bartosz.switala.01@gmail.com>
2024-05-05 13:51:39 +0200

    1 line change

--- src/main/java/karuna/karuna_backend/Authentication/CustomUserDetails.java
+++ src/main/java/karuna/karuna_backend/Authentication/CustomUserDetails.java
@@ -19,7 +19,7 @@ public class CustomUserDetails implements UserDetails {
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return user.getRoles().stream()
                 .map(role -> new SimpleGrantedAuthority(role.getName()))
-                .collect(Collectors.toList());
+                .toList();
     }
 
     @Override
