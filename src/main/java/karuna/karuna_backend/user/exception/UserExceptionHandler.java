0474af76a9ddfcc1a5176d1b78df52bce2760492
karolwolny <kwolny31@gmail.com>
2024-05-06 19:27:23 +0200

    separate exception handlers

--- src/main/java/karuna/karuna_backend/user/exception/UserExceptionHandler.java
+++ src/main/java/karuna/karuna_backend/user/exception/UserExceptionHandler.java
@@ -1,22 +1,13 @@
 package karuna.karuna_backend.user.exception;
 
-import karuna.karuna_backend.exception.CustomExceptionKey;
 import karuna.karuna_backend.exception.dto.CustomErrorResponse;
-import karuna.karuna_backend.user.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class RestExceptionHandler {
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    CustomErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException) {
-        return new CustomErrorResponse(CustomExceptionKey.UNIQUE_CONSTRAINT_VIOLATION, methodArgumentNotValidException.getBindingResult().getAllErrors().get(0).getDefaultMessage());
-    }
+class UserExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
