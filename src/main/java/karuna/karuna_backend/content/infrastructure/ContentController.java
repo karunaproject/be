ffeb84f46000e2cc31e7539ba72914e7dfe6ed6c
karolwolny <kwolny31@gmail.com>
2024-05-09 21:15:14 +0200

    remove contenst/specific-path wrong intepretation

--- src/main/java/karuna/karuna_backend/content/infrastructure/ContentController.java
+++ src/main/java/karuna/karuna_backend/content/infrastructure/ContentController.java
@@ -39,7 +39,7 @@ class ContentController {
             responseCode = "200",
             description = "Updated contents",
             content = @Content(mediaType = "application/json", schema = @Schema(implementation = MassContentWrapper.class)))
-    @PutMapping("/specific-path")
+    @PutMapping
     MassContentWrapper massUpdateContent(@RequestBody MassContentWrapper massContentWrapper) {
         return contentService.massUpdateContent(massContentWrapper);
     }
