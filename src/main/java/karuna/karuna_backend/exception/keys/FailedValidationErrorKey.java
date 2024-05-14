455c45d70d737c53638e683b8e872768656898de
karolwolny <kwolny31@gmail.com>
2024-05-14 15:55:44 +0200

    change keys and add swagger docs

--- src/main/java/karuna/karuna_backend/exception/keys/FailedValidationErrorKey.java
+++ src/main/java/karuna/karuna_backend/exception/keys/FailedValidationErrorKey.java
@@ -1,13 +1,24 @@
 package karuna.karuna_backend.exception.keys;
 
-//TODO: SWAGGER SCHEMA AFTER DISCUSS ABOUT NAMES OF KEYS
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Defines validation error types. " +
+        "SIZE_INVALID: Invalid size of field. " +
+        "PATTERN_INVALID: Invalid pattern of field. " +
+        "EMAIL_INVALID: Invalid email format. " +
+        "MORE_THAN_MAX: Value exceeds maximum allowed. " +
+        "MIN_NOT_REACHED: Value does not meet minimum required. " +
+        "INVALID_NULL_FIELD: Field cannot be null. " +
+        "INVALID_BLANK_FIELD: Field cannot be blank. " +
+        "UNKNOWN: Unknown validation error."
+)
 public enum FailedValidationErrorKey {
-    SIZE,
-    PATTERN,
-    EMAIL,
-    MAX,
-    MIN,
-    NOT_NULL,
-    NOT_BLANK,
+    SIZE_INVALID,
+    PATTERN_INVALID,
+    EMAIL_INVALID,
+    MORE_THAN_MAX,
+    MIN_NOT_REACHED,
+    INVALID_NULL_FIELD,
+    INVALID_BLANK_FIELD,
     UNKNOWN
 }
--- src/main/java/karuna/karuna_backend/exception/validation/ValidationKeyResolver.java
+++ src/main/java/karuna/karuna_backend/exception/validation/ValidationKeyResolver.java
@@ -17,19 +17,19 @@ public class ValidationKeyResolver {
         if (Objects.nonNull(annotationType)) {
             switch (annotationType.toLowerCase()) {
                 case "size":
-                    return FailedValidationErrorKey.SIZE;
+                    return FailedValidationErrorKey.SIZE_INVALID;
                 case "notblank":
-                    return FailedValidationErrorKey.NOT_BLANK;
+                    return FailedValidationErrorKey.INVALID_BLANK_FIELD;
                 case "notnull":
-                    return FailedValidationErrorKey.NOT_NULL;
+                    return FailedValidationErrorKey.INVALID_NULL_FIELD;
                 case "min":
-                    return FailedValidationErrorKey.MIN;
+                    return FailedValidationErrorKey.MIN_NOT_REACHED;
                 case "max":
-                    return FailedValidationErrorKey.MAX;
+                    return FailedValidationErrorKey.MORE_THAN_MAX;
                 case "email":
-                    return FailedValidationErrorKey.EMAIL;
+                    return FailedValidationErrorKey.EMAIL_INVALID;
                 case "pattern":
-                    return FailedValidationErrorKey.PATTERN;
+                    return FailedValidationErrorKey.PATTERN_INVALID;
             }
         }
         return FailedValidationErrorKey.UNKNOWN;
