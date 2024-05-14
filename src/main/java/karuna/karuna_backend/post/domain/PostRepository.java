748a2fdc0a294c63a9dc681db5a27ef4596a8891
karolwolny <kwolny31@gmail.com>
2024-05-14 19:23:40 +0200

    Remove @Repository is unused when extends JpaRepostiory, make VisitorsMessages public

--- src/main/java/karuna/karuna_backend/post/domain/PostRepository.java
+++ src/main/java/karuna/karuna_backend/post/domain/PostRepository.java
@@ -1,7 +1,5 @@
 package karuna.karuna_backend.post.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 interface PostRepository extends JpaRepository<Post, Long> { }
