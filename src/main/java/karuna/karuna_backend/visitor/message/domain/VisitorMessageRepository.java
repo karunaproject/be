3946d81bff56a9fabf6e2508c79a5adde04bf280
KrystynaCal <krystynacal5@gmail.com>
2024-05-14 10:52:43 +0200

    add stereotype Repository

--- src/main/java/karuna/karuna_backend/visitor/message/domain/VisitorMessageRepository.java
+++ src/main/java/karuna/karuna_backend/visitor/message/domain/VisitorMessageRepository.java
@@ -1,9 +1,11 @@
 package karuna.karuna_backend.visitor.message.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface VisitorMessageRepository extends JpaRepository<VisitorMessage, Long> {
     List<VisitorMessage> findAllByOrderByDateDesc();
 }
