426ff23e61021c85d30c9cd0bb3d825b1c9d1cef
karolwolny <kwolny31@gmail.com>
2024-05-15 20:18:06 +0200

    Add @Repository

--- src/main/java/karuna/karuna_backend/content/domain/ContentRepository.java
+++ src/main/java/karuna/karuna_backend/content/domain/ContentRepository.java
@@ -1,9 +1,11 @@
 package karuna.karuna_backend.content.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 interface ContentRepository extends JpaRepository<Content, Long> {
     List<Content> findByPageIgnoreCaseOrPageNull(String page);
 }
--- src/main/java/karuna/karuna_backend/post/domain/PostRepository.java
+++ src/main/java/karuna/karuna_backend/post/domain/PostRepository.java
@@ -1,5 +1,7 @@
 package karuna.karuna_backend.post.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 interface PostRepository extends JpaRepository<Post, Long> { }
