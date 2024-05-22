74e5cc6f37ed5685eaf25c25b15bfdf75edcb8bd
Luzu4 <maciekluz@gmail.com>
2024-05-23 00:26:30 +0200

    ticket-24 | Key description

--- src/main/java/karuna/karuna_backend/exception/keys/ConstraintViolationKey.java
+++ src/main/java/karuna/karuna_backend/exception/keys/ConstraintViolationKey.java
@@ -1,5 +1,11 @@
 package karuna.karuna_backend.exception.keys;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Defines constrain violation error types. " +
+        "INVALID_EMAIL_PATTERN: invalid email pattern. " +
+        "LENGTH_IS_INCORRECT: Invalid length of variable. " +
+        "UNKNOWN_ERROR: An unknown error occurred.")
 public enum ConstraintViolationKey {
     UNKNOWN_ERROR,
     INVALID_EMAIL_PATTERN,
