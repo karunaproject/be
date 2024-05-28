9d6f37d288d27ad5657e958ece0e2c5077cc1aea
Luzu4 <maciekluz@gmail.com>
2024-05-28 22:48:56 +0200

    ticket-24 | removed wildcards

--- src/main/java/karuna/karuna_backend/receiver/infrastrucutre/ReceiverController.java
+++ src/main/java/karuna/karuna_backend/receiver/infrastrucutre/ReceiverController.java
@@ -15,7 +15,12 @@ import karuna.karuna_backend.receiver.dto.ReceiversDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
--- src/test/java/karuna/karuna_backend/receiver/domain/InMemoryReceiverRepository.java
+++ src/test/java/karuna/karuna_backend/receiver/domain/InMemoryReceiverRepository.java
@@ -1,13 +1,15 @@
 package karuna.karuna_backend.receiver.domain;
 
-import karuna.karuna_backend.receiver.dto.ReceiverDTO;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.query.FluentQuery;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 
 public class InMemoryReceiverRepository implements ReceiverRepository {
