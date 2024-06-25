082d2a582893146ae1ab1f9e1b4b7631daf0a59e
KrystynaCal <krystynacal5@gmail.com>
2024-06-25 14:49:55 +0200

    ticket-49 | add authentication context

--- src/test/java/karuna/karuna_backend/post/domain/PostServiceTest.java
+++ src/test/java/karuna/karuna_backend/post/domain/PostServiceTest.java
@@ -5,6 +5,9 @@ import karuna.karuna_backend.post.dto.PostCreateDto;
 import karuna.karuna_backend.post.dto.PostDto;
 import karuna.karuna_backend.utils.AuthenticationUtil;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -12,18 +15,26 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 class PostServiceTest {
 
     private final PostService postService = PostTestConfiguration.postService();
+    private Authentication authentication;
 
     @Test
     void shouldCreatePost() {
-        //when: When create post, returns created post
+        // given: Set up authentication context for the test user
+        authentication = new TestingAuthenticationToken(
+                Constants.USERNAME,
+                null,
+                Constants.ROLE_USER);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        //when: When create a new post
         PostCreateDto postCreateDto = new PostCreateDto(Constants.BODY);
         PostDto postDto = postService.createPost(postCreateDto);
         String username = AuthenticationUtil.getUsername();
-        //then: Check if  database give id and correctly save data
+        //then: Check if database give ID and correctly save post
         assertEquals(1L, postDto.id());
         assertNotNull(postDto.createdAt());
         assertEquals(Constants.BODY, postDto.body());
         assertEquals(username, postDto.author());
+        SecurityContextHolder.clearContext();
     }
 
 }
