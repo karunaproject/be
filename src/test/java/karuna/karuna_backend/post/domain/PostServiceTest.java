198b0ca6f9dd95ad73d7900a11b1aba7ea3aa47f
KrystynaCal <krystynacal5@gmail.com>
2024-06-25 22:14:50 +0200

    ticket-46 | review fix

--- src/test/java/karuna/karuna_backend/post/domain/PostServiceTest.java
+++ src/test/java/karuna/karuna_backend/post/domain/PostServiceTest.java
@@ -14,7 +14,6 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PostServiceTest {
 
